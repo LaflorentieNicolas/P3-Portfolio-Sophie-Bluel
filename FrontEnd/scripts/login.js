80996c135e3a0d9ee14579153ac4c1e7d4029474
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-01-02 00:19:43 +0100

    add login screen

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,1 +1,5 @@
-const API_LOGIN = 
+const API_LOGIN = "http://localhost:5678/api/users/login";
+
+const form = document.getElementById("form-login");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
